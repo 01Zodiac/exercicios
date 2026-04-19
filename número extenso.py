3b416cd3f3e7ab4c5a3aaf48f55dc8ab35c814a0
_Zodiac01 <lucasmasterls649@gmail.com>
2026-04-19 12:35:30 -0300

    Add files via upload

---
+++ número extenso.py
@@ -0,0 +1,9 @@
+cont = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete",
+        "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze",
+        "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+while True:
+    num = int(input("Digite um número entre 0 e 20: "))
+    if 20 >= num >= 0:
+        break  
+    print("Escolha um número válido.")
+print(cont[num])
