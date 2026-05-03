fab9a19461a37b32f3d30ff345882131e5791385
_Zodiac01 <lucasmasterls649@gmail.com>
2026-05-03 02:53:47 -0300

    IA

---
+++ dados no dicionário.py
@@ -0,0 +1,43 @@
+dados = dict()
+pessoas = list()
+soma = 0
+while True:
+    dados.clear()
+    dados["nome"] = input("Nome: ")
+    while True:
+        dados["sexo"] = input("Sexo[M / F]: ").upper()
+        if dados["sexo"] in "MF":
+            break
+        else:
+            print("Opção inválida")
+    while True:
+        dados["idade"] = int(input("Idade: "))
+        if dados["idade"] < 0:
+            print("Idade inválida")
+        else:
+            soma += dados["idade"]
+            break
+    pessoas.append(dados.copy())
+    while True:
+        opc = input("Quer continuar? [S / N]: ").upper()
+        if opc in "SN":
+            break
+        else:
+            print("Opção inválida")
+    if opc == "N":
+        break
+print(f"A) Ao todo temos {len(pessoas)} pessoas cadastradas.")
+media = soma / len(pessoas)
+print(f"B) A média de idade é de {media} anos.")
+print(f"C) As mulheres cadastradas foram: ", end="")
+for i in range(0, len(pessoas)):
+    if pessoas[i]["sexo"] == "F":
+        print(f"{pessoas[i]["nome"]} ", end="")
+print(f"\nD) Lista de pessoas que estão acima da média:")
+for i in range(0, len(pessoas)):
+    if pessoas[i]["idade"] >= media:
+        print("    -", end="")
+        for entrad, said in pessoas[i].items():
+            print(f" {entrad}: {said}", end=";")
+        print()
+print("Encerrado.")
