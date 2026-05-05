acf74c0e4fc68374bf913d3e7799690342721539
_Zodiac01 <lucasmasterls649@gmail.com>
2026-05-04 22:28:03 -0300

    saborr

---
+++ área.py
@@ -0,0 +1,6 @@
+largura = float(input("Largura(m): "))
+comprimento = float(input("Comprimento(m): "))
+def área():
+    area = (largura * comprimento)
+    print(f"A área de {largura}x{comprimento} é de {area}")
+área()
